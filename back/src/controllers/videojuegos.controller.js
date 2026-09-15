1f77857996cc8d9fcfe460c68a92a1cac02cdd34
Gustavo <gustavolopez819@gmail.com>
2026-09-15 14:27:22 -0500

    feat(consultar-videojuegos): endpoint get para consultar videjuegos

--- back/src/controllers/videojuegos.controller.js
+++ back/src/controllers/videojuegos.controller.js
@@ -1,4 +1,10 @@
-import { crearVideojuego } from '../services/videojuegos.service.js';
+import { crearVideojuego, obtenerVideojuegos } from '../services/videojuegos.service.js';
+
+export async function consultarVideojuegos(req, res) {
+  const videojuegos = await obtenerVideojuegos();
+
+  res.status(200).json({ videojuegos });
+}
 
 export async function registrarVideojuego(req, res) {
   const videojuego = await crearVideojuego(req.body);
